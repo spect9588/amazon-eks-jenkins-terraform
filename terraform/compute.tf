ffca93c05b386d9d885eeb5189151a99417ae2aa
Max Kitgor <mkitgor@anexinet.com>
2020-02-04 09:02:25 -0500

    formatting

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,15 +1,18 @@
-
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
+
   filter {
     name = "name"
+
     values = [
       "amzn2-ami-hvm-*-x86_64-gp2",
     ]
   }
+
   filter {
     name = "owner-alias"
+
     values = [
       "amazon",
     ]
@@ -17,16 +20,19 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 resource "aws_instance" "jenkins-instance" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type   = "t2.medium"
-  key_name        = "${var.keyname}"
+  ami           = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type = "t2.medium"
+  key_name      = "${var.keyname}"
+
   #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+  subnet_id              = "${aws_subnet.public-subnet-1.id}"
+
   #name            = "${var.name}"
   user_data = "${file("install_jenkins.sh")}"
 
   associate_public_ip_address = true
+
   tags = {
     Name = "Jenkins-Instance"
   }
@@ -52,10 +58,10 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
-
 provider "aws" {
-  region                  = "${var.region}"
+  region = "${var.region}"
+
   # shared_credentials_file = "~/.aws/credentials"
   # profile                 = "development"
 }
-
